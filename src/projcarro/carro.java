ebed2c45ed3ed75b97ea861019710071d31724fa
RAÍSSA THIELY TOBIAS DA CRUZ <26147962@sempreunama.com.br>
2019-04-09 19:28:06 -0300

    Initial commit

---
+++ src/projcarro/carro.java
@@ -0,0 +1,116 @@
+package projcarro;
+
+public class carro {
+    //Atributos
+    String modelo,cor,marca,chassi;
+    int ano, numMarchas, marchaAtual,numPortas;
+    double velocidademax, velocidadeAtual, volcombustivel;
+    boolean tetoSolar;
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo{String modelo[this.modelo = modelo;}
+
+    public String getCor() {
+        return cor;
+    }
+    public void setCor(String cor {
+        this.cor = cor;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getChassi() {
+        return chassi;
+    }
+
+    public void setChassi(String chassi) {
+        this.chassi = chassi;
+    }
+
+    public int getAno() {
+        return ano;
+    }
+
+    public void setAno(int ano) {
+        this.ano = ano;
+    }
+
+    public int getNumMarchas() {
+        return numMarchas;
+    }
+
+    public void setMarchaAtual(int marchaAtual) {
+        this.marchaAtual = marchaAtual;
+    }
+
+    public int getNumPortas() {
+        return numPortas;
+    }
+
+    public void setNumPortas(int numPortas) {
+        this.numPortas = numPortas;
+    }
+
+    public double getVelocidademax() {
+        return velocidademax;
+    }
+
+    public void setVelocidademax(double velocidademax) {
+        this.velocidademax = velocidademax;
+    }
+    public double getVelocidadeAtual() {
+        return velocidadeAtual;
+    }
+
+    public void setVelocidadeAtual(double velocidadeAtual) {
+        this.velocidadeAtual = velocidadeAtual;
+    }
+
+    public double getVolcombustivel() {
+        return volcombustivel;
+    }
+
+    public void setVolcombustivel(double volcombustivel) {
+        this.volcombustivel = volcombustivel;
+    }
+
+    public boolean isTetoSolar() {
+        return tetoSolar;
+    }
+
+    public void setTetoSolar(boolean tetoSolar) {
+        this.tetoSolar = tetoSolar;
+    }
+    public Proprietario getproprietario (){
+        return Proprietario;
+    }
+
+    public void setproprietario(Proprietario proprietario) {
+        this.Proprietario = proprietario;}
+    Proprietario proprietario;
+    }
+    //Metodos
+void acelera {} { velocidadeAtual += 1;}
+
+void freia {} { velocidade atual += 1;}
+void trocaMaecha {int novaMarcha} {
+
+      if (novaMarcha > numMarcha){
+        System.out.println("Não existe marcha");
+        }else{
+          marchaAtual =  nova marcha;
+        }
+
+
+
+
+}
---
+++ src/projcarro/proprietario.java
@@ -0,0 +1,15 @@
+package projcarro;
+
+public class proprietario {
+    String nome, datanascimento, rua ,bairro , cidade, estado,complemento;
+    int cpf, rg, cep;
+    public proprietario ( String nome , int cpf){
+   public String getNome{}{ return nome;}
+   public String datanascimento 
+
+
+
+
+
+    }
+
