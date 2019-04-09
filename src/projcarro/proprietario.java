209e0b114f0ce508206010959d1eb8baddff4967
RTHIELYS <47646980+RTHIELYS@users.noreply.github.com>
2019-04-09 20:05:39 -0300

    Update proprietario.java

--- src/projcarro/proprietario.java
+++ src/projcarro/proprietario.java
@@ -4,8 +4,35 @@ public class proprietario {
     String nome, datanascimento, rua ,bairro , cidade, estado,complemento;
     int cpf, rg, cep;
     public proprietario ( String nome , int cpf){
+}
    public String getNome{}{ return nome;}
-   public String datanascimento 
+      public void setNome { String nome } {this.nome = nome;}
+   public String getDatanascimento {} { return datanascimento;}
+        public void setDatanascimento { String datanascimento } {this.datanascimento =datanascimento;}
+        public String getRua {} {return rua;}
+      public void setRua{ String Rua } {this.rua = rua;}
+         public String getBairro{}{ return bairro;}
+     public void setBairro{ String Bairro } {this.bairro = bairro;}
+     public String getCidade{}{ return Cidade;}
+     public void setCidade{ String Cidade} {this.Cidade = Cidade
+         public String getEstado {}{ returnEstado ;}
+     public void setEstado{ String Estado } {this.Estado  = Estado ;}
+         public String getComplemento {}{ returnComplemento ;}
+     public void setComplemento{ String Complemento} {this.Complemento  = Complemento ;}
+                                            
+        public int cpf getCpf{} {return cpf;}
+       public void setCpf{ String Cpf} {this.Cpf = Cpf;}
+        public int rg getRg{} {return Rg;}
+       public void setRg{ String Rg} {this.Rg = Rg;}
+        public int cep getCep{} {return cep;}
+       public void setCep{ String Cep} {this.Cep = Cep;}
+                                            
+                                            
+                                            
+    
+    
+    
+        
 
 
 
